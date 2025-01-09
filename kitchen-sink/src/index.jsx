78a9343c14566ecb9369f83521febafeda16657a
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-09 15:13:33 +0800

    Fix kitchen-sink

--- kitchen-sink/src/index.jsx
+++ kitchen-sink/src/index.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './main.css';
+
+const projects = import.meta.glob('./tests/*', {
+  eager: false,
+});
+
+console.log(projects);
 
 const PROJECT = 'main';
 
 export const root = ReactDOM.createRoot(document.getElementById('root'));
 const render = async () => {
-  const Component = (
-    await import(
-      process.env.NODE_ENV !== 'production' ? `./${PROJECT}.jsx` : './main.jsx'
-    )
-  ).default;
+  const Component = (await projects[`./tests/${PROJECT}.jsx`]()).default;
   root.render(<Component />);
 };
 
